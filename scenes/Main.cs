5a4e0d07fe9694c1a301103facf5515fd6643afc
Duarte-753 <julioduartebatista753@gmail.com>
2026-05-09 17:13:04 -0300

    Primeiro Script em CSharp

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,24 +1,25 @@
 using Godot;
 using System;
 
+namespace Game;
+
 public partial class Main : Node2D
 {
+	private Sprite2D sprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var testNumber = 42;
-		var testArray = new string[] { "Hello", "World" };
-		TestMethod();
-		GD.Print("Hello World from C#!");
+		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
-
-	private void TestMethod()
-	{
-		GD.Print("This is a test method.");
+		var mousePosition = GetGlobalMousePosition();
+		var gridPosition = mousePosition / 64;
+		gridPosition = gridPosition.Floor();
+		GD.Print(gridPosition);
+		sprite.Position = gridPosition * 64;
 	}
 }
